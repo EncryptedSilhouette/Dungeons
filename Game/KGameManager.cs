ff3517ffdc479a1d83f6eeeed8c9a4b486e35572
EncryptedSilhouette <prestongregory125@gmail.com>
2026-03-31 12:22:29 -0700

    wip

--- Game/KGameManager.cs
+++ Game/KGameManager.cs
@@ -1,7 +1,7 @@
-using System.Net.NetworkInformation;
-using System.Reflection.Metadata;
-using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
 using System.Security.Cryptography.X509Certificates;
+using Microsoft.VisualBasic;
+using SFML.Graphics;
 using SFML.System;
 
 public class KGameManager
@@ -52,86 +52,94 @@ public enum KChunkPriority : byte
     HIGH,
 }
 
-public struct KGameChunck
+public struct KGameChunk
 {
-    public static ulong GetHandle(uint x, uint y) => x + y * uint.MaxValue;
-    public static Vector2u GetPosition(ulong handle) => new Vector2u
+    public const int CHUNK_SIZE = 16;
+
+    #region Static
+    public static ulong GetHandle(long x, long y)
     {
-        X = (uint)(handle % uint.MaxValue),
-        Y = (uint)(handle / uint.MaxValue),
-    };
+        //removes negative value range to fit uint32 range.
+        x -= int.MinValue;
+        y -= int.MinValue;
+        //Converts from grid coords to handle.
+        return (ulong)x + (ulong)y * uint.MaxValue;
+    }
+
+    public static Vector2i GetPosition(ulong handle)
+    {
+        //Convert to world coords.
+        long x = (long)(handle % uint.MaxValue);
+        long y = (long)(handle / uint.MaxValue);
+        //Add the negative value range to fit to sint32 range.
+        x = x + int.MinValue;
+        y = y + int.MinValue;
+        //should cast safely x_x.
+        return new((int)x, (int)y);
+    }
+    #endregion
 
     public ulong Handle;
     public bool Initialized;
     public KChunkPriority Priority;
+    public Vector2i Position => GetPosition(Handle);
 
-    public Vector2u Position => GetPosition(Handle);
-
-    public KGameChunck(ulong handle)
+    public KGameChunk(ulong handle)
     {
         Handle = handle;
         Initialized = false;
         Priority = KChunkPriority.NONE;
     }
+
+    public KGameChunk(int x, int y)
+    {
+        Handle = GetHandle(x, y);
+        Initialized = false;
+        Priority = KChunkPriority.NONE;
+    }
 }
 
- public struct KChunkRegion
+public struct KChunkRegion
 {
-    public uint X;
-    public uint Y;
-    public uint Width;
-    public uint Height;
-    public KBufferRegion Region;
-    
-    public KChunkRegion(uint x, uint y, uint width, uint height)
+    public KBufferRegion Region; 
+    public IntRect Area;
+    public KChunkHandler ChunkHandler;
+    public Span<KGameChunk> Chunks => ChunkHandler.ChunkBuffer.AsSpan((int)Region.Offset, (int)Region.Capacity);
+
+    public KChunkRegion(KChunkHandler handler, KBufferRegion region, Vector2i pos, Vector2i size)
     {
-        X = x;
-        Y = y;
-        Width = width;
-        Height = height;
+        Region = region;
+        Area = new(pos, size);
+        ChunkHandler = handler;
     }
+
+    public bool ContainsChunk(ulong handle) => Area.Contains(KGameChunk.GetPosition(handle));
+    public bool ContainsChunk(Vector2i point) => Area.Contains(point);
 }
 
-public class KChunkManager
+public class KChunkHandler
 {
-    
-
-    public KGameChunck[] ChunkHeap;
-
-    //public Thread HeapThread;
-    //public Thread LoadingThread;
-
-    public KChunkManager()
-    {
-        
-    }
-
-    public void LoadChunk(ulong handle)
-    {
-        
-    }
-
-    public void LoadChunkFromFile()
-    {
-        
-    }
-
-    public void LoadChunkRegion(in KChunkRegion region)
-    {
-        
-    }
+    public Dictionary<ulong, KGameChunk> chunkCache = new();
 }
 
+public struct KPlayerChunkCluster
+{
+    public int LoadDist;
+    public int SimDist;
+
+    public Action LoadChunkRegion;
+
+}
 
 public class KGameWorld
 {
     public KPlayer Player;
-    public KChunkManager ChunkManager;
+
 
     public KGameWorld(KPlayer player)
     {
         Player = player;
-        ChunkManager = new();
+
     }   
 
     public void Init()
@@ -148,35 +156,4 @@ public class KGameWorld
     {
         
     }
-
-    public void Generate(uint x, uint y, uint width, uint height)
-    {
-        KChunkRegion chunkRegion = new(x, y, width, height);
-    }
 }
-
-public class KWorldgenerator()
-{
-   
-}
-
-//public struct KHeap<T>
-//{
-//    public int HEAP_SIZE = 128;
-//    public T[] Contents;
-
-//    public KHeap(int size = HEAP_SIZE)
-//    {
-//        Contents = new T[HEAP_SIZE];
-//    }
-
-//    public KBufferRegion Rent(KBufferRegion region, int amount)
-//    {
-//        for (uint i = region.Offset; i < (region.Offset + region.Capacity); i++)
-//        {
-//            if (Contents[i].Initialized) region.Offset = i + 1;
-//            else if ((i - region.Offset) == amount) return region;
-//        }
-//        return region;
-//    }
-//}
